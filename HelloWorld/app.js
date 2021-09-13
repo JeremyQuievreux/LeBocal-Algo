a461b0d2f9c8189663ed310ec26bcd37cf02a7b6
DevHÿllen <64634619+Jequi59@users.noreply.github.com>
2021-09-13 13:33:22 +0200

    update

---
+++ HelloWorld/app.js
@@ -0,0 +1,6 @@
+let nom = "Quievreux";
+let prenom = "Jérémy";
+let age = 36;
+let mail = "jquievreux.devpro@gmail.com";
+
+console.log( "Hello World, je m'appelle " +nom +" "+ prenom +", j'ai " + age + " ans, mon mail est " + mail);
---
+++ Resto1/app.js
@@ -0,0 +1,56 @@
+let age = window.prompt("Quelle est votre age ?");
+
+let credit = window.prompt("De combien voulez vous créditer votre compte ?")
+
+console.log("Carte :");
+console.log("1 - Boisson non-alcoolisé : 5€");
+console.log("2 - Boisson alcoolisé : 8€");
+console.log("3 - Plat : 20€");
+
+let choix = window.prompt("Faite votre choix :")
+
+if (choix == 1) {
+    if (credit >= 5) {
+        credit = credit - 5
+        console.clear()
+        console.log("Commande acceptée");
+        console.log("Bon appetit");
+    } else {
+        console.clear()
+        console.log(" Commande refusée, crédit insuffisant");
+    }
+}
+
+if (choix == 2) {
+    if (age < 18) {
+        console.clear()
+        console.log("Commande refusée, vous n'avez pas l'age légal");
+    } else if (credit < 8){
+        console.clear()
+        console.log("Commande refusée, vous n'avez pas assez de credit");
+    } else {
+        credit = credit - 18
+        console.clear()
+        console.log("Commande acceptée");
+        console.log("Bon appetit");
+    }
+}
+
+if (choix ==3) {
+    if (credit >= 20) {
+        credit = credit - 20
+        console.clear()
+        console.log("Commande acceptée");
+        console.log("Bon appetit");
+    } else {
+        console.clear()
+        console.log("Commande refusée, vous n'avez pas asser de crédit");
+    }    
+}
+
+if (choix != 1 || choix != 2 || choix != 3) {
+    console.clear();
+    console.log("Nous n'avons compris votre demande");    
+}
+
+console.log("Vous avez " + credit + " € sur votre compte.");
