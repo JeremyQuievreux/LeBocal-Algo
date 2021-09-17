d849682bafec87a1714dde71a046d0d043b63e74
DevHÿllen <64634619+Jequi59@users.noreply.github.com>
2021-09-17 23:28:18 +0200

    vendredi 17/9 by night

--- TPCarnet/V2/app.js
+++ TPCarnet/V2/app.js
@@ -1,60 +1,80 @@
 //Création du tableau vide
 let contacts = [];
 //Création des variables
+//Les input
 let nameInputElt = document.getElementById("name");
 let lastnameInputElt = document.getElementById("lastname");
 let mailInputElt = document.getElementById("mail");
 let telInputElt = document.getElementById("phone");
 let validerBtn = document.querySelector(".validerBtn");
-let contactsElt = document.querySelector(".list-items");
 let findInputElt = document.getElementById("find-bar");
-
-let loadLStorage = localStorage.getItem("contact");
-//Condition de départ pour les données locales
-if (loadLStorage) {
-    let loadLStorageParse = JSON.parse(loadLStorage)
-    contacts = loadLStorageParse;
-    loadLStorageParse.forEach((contact) => {
-        //Création des elements   
-        let newdivElt = document.createElement("div");
-        let newPElt = document.createElement("p");
-        let newCloseBtn = document.createElement("button");
-        //Configuration du boutton
-        newCloseBtn.innerText = "X";
-        newCloseBtn.classList.add("close-btn");
-        //Setup de la div
-        newdivElt.classList.add("item");
-        //Setup du p
-        newPElt.innerText = `Je suis ${contact.name} ${contact.lastname} mon numéro est le ${contact.phone}, et voila mon adresse mail ${contact.mail}`;
-        //Injection des elements dans la nouvelle div
-        newdivElt.appendChild(newPElt);
-        newdivElt.appendChild(newCloseBtn);
-        //Injection de la div dans la grosse div
-        contactsElt.appendChild(newdivElt);
-        //Event au click sur le boutton fermé
-        newCloseBtn.addEventListener("click", () => {
-            //suppression de l'element dans le array
-            //Recherche de l'index du contact grace a l'id de l'user
-            const findById = (element) => element.id == contact.id;
-            let index = contacts.findIndex(findById);
-            //Suppression de l'élément dans l'array grace a l'index
-            contacts.splice(index, 1);
-            //Mise a Jour dans le localStorage
-            saveOnLocal();
-            //suppression de la div
-            newdivElt.remove()
-        }) 
-    });
-};
+//Main Div
+let contactsElt = document.querySelector(".list-items");
+//Mise sous variable du localStorage("contacts")
+let loadLStorage = localStorage.getItem("contacts");
+//*****FONCTION*****//
+//Fonction de création d'une div contact
+function createContactDiv(contact) {
+    //Création des elements   
+    let newdivElt = document.createElement("div");
+    let newPElt = document.createElement("p");
+    let newCloseBtn = document.createElement("button");
+    //Configuration du boutton
+    newCloseBtn.innerText = "X";
+    newCloseBtn.classList.add("close-btn");
+    //Setup de la div
+    newdivElt.classList.add("item");
+    //Setup du p
+    newPElt.innerText = `Je suis ${contact.name} ${contact.lastname} mon numéro est le ${contact.phone}, et voila mon adresse mail ${contact.mail}`;
+    //Injection des elements dans la nouvelle div
+    newdivElt.appendChild(newPElt);
+    newdivElt.appendChild(newCloseBtn);
+    //Injection de la div dans la grosse div
+    contactsElt.appendChild(newdivElt);
+    //Event au click sur le boutton fermé
+    newCloseBtn.addEventListener("click", () => {
+        //suppression de l'element dans le array
+        //Recherche de l'index du contact grace a l'id de l'user
+        const findById = (element) => element.id == contact.id;
+        let index = contacts.findIndex(findById);
+        //Suppression de l'élément dans l'array grace a l'index
+        contacts.splice(index, 1);
+        //Mise a Jour dans le localStorage
+        saveOnLocal();
+        //suppression de la div
+        newdivElt.remove()
+    })
+}
 //Fonction de sauvegarde en local
 function saveOnLocal(){
     //Stingification de l'array
     let contactsString = JSON.stringify(contacts);
     //Sauvegarde en localStorage
-    localStorage.setItem("contact",contactsString);
+    localStorage.setItem("contacts",contactsString);
     //debug Perso
-    console.log(localStorage.getItem("contact"));
+    console.log(localStorage.getItem("contacts"));
 }
+//Fonction de vérification contact.name VS input.value
+function verifierCorrespondance(contact) {
+    //Normalisation du regex et du prénom pour la comparaison
+    let regex = new RegExp(`${findInputElt.value.toLowerCase()}`);
+    let result = regex.test(contact.lastname.toLowerCase());
+    if (result) {
+        return contact;
+    }
+}
+//Check a l'ouverture de la page si il y a des données contacts
+//dans le LStorage
+if (loadLStorage) {
+    //Parse des données dans le LStorage
+    let loadLStorageParse = JSON.parse(loadLStorage)
+    //Remplissage du array vide avec les données du LStorage
+    contacts = loadLStorageParse;
+    //Boucle sur l'array pour faire les sous div
+    loadLStorageParse.forEach((contact) => {
+        createContactDiv(contact);
+    });
+};
 //Event au click sur le btn valider
 validerBtn.addEventListener('click', function(){
     //longueur de l'array
@@ -75,35 +95,7 @@ validerBtn.addEventListener('click', function(){
     contactsElt.innerHTML = "";
     //Boucle sur l'array (pour chaque)
     contacts.forEach((contact) => {
-        //Création des elements   
-        let newdivElt = document.createElement("div");
-        let newPElt = document.createElement("p");
-        let newCloseBtn = document.createElement("button");
-        //Configuration du boutton
-        newCloseBtn.innerText = "X";
-        newCloseBtn.classList.add("close-btn");
-        //Setup de la div
-        newdivElt.classList.add("item");
-        //Setup du p
-        newPElt.innerText = `Je suis ${contact.name} ${contact.lastname} mon numéro est le ${contact.phone}, et voila mon adresse mail ${contact.mail}`;
-        //Injection des elements dans la nouvelle div
-        newdivElt.appendChild(newPElt);
-        newdivElt.appendChild(newCloseBtn);
-        //Injection de la div dans la grosse div
-        contactsElt.appendChild(newdivElt);
-        //Event au click sur le boutton fermé
-        newCloseBtn.addEventListener("click", () => {
-            //suppression de l'element dans le array
-            //Recherche de l'index du contact grace a l'id de l'user
-            const findById = (element) => element.id == contact.id;
-            let index = contacts.findIndex(findById);
-            //Suppression de l'élément dans l'array grace a l'index
-            contacts.splice(index, 1);
-            //Mise a Jour dans le localStorage
-            saveOnLocal();
-            //suppression de la div
-            newdivElt.remove()
-        }) 
+        createContactDiv(contact);        
     });
     //reset des champs
     nameInputElt.value = "";
@@ -111,28 +103,26 @@ validerBtn.addEventListener('click', function(){
     mailInputElt.value = "";
     telInputElt.value = "";
 });
-
-
-
-findInputElt.addEventListener("input", () => {
+//Event dans la barre de recherche
+findInputElt.addEventListener("input", () => {    
+    //Création et reset du tableau avec les contacts filtrés
     let filteredContacts = "";
-    function verifierCorrespondance(contact) {
-        let regex = new RegExp(`${findInputElt.value}`);
-        let result = regex.test(contact.name);
-        if (result) {
-            return contact;
-        }
-    }    
+    //Modification du tableau des contacts filtrés    
     filteredContacts = contacts.filter(verifierCorrespondance);
-    console.log(filteredContacts);
-
     //reset la main div
-    //faire de des sous avec l'array de sortie
+    contactsElt.innerHTML="";
+    filteredContacts.forEach((contact) => {
+        createContactDiv(contact);
+    });
 });
-
+//Event au blur de l'input de recherche
 findInputElt.addEventListener('blur', () => {
-    console.log("blur");
     findInputElt.value = "";
-    //reset la main div
-    //refaire des sous div avec les contacts du localstorage
+    contacts.forEach((contact) => {
+        createContactDiv(contact);
+    });
 })
+
+
+let findLow = findInputElt.value.toLowerCase();
+    console.log(findLow);
---
+++ TPCarnet/V2/app2.js
@@ -0,0 +1,201 @@
+//A faire lowcase le input value de la barre de recherche
+
+//Création du tableau vide
+let contacts = [];
+//Création des variables
+let nameInputElt = document.getElementById("name");
+let lastnameInputElt = document.getElementById("lastname");
+let mailInputElt = document.getElementById("mail");
+let telInputElt = document.getElementById("phone");
+let validerBtn = document.querySelector(".validerBtn");
+let contactsElt = document.querySelector(".list-items");
+let findInputElt = document.getElementById("find-bar");
+
+let loadLStorage = localStorage.getItem("contact");
+//Condition de départ pour les données locales
+if (loadLStorage) {
+    let loadLStorageParse = JSON.parse(loadLStorage)
+    contacts = loadLStorageParse;
+    loadLStorageParse.forEach((contact) => {
+        //Création des elements   
+        let newdivElt = document.createElement("div");
+        let newPElt = document.createElement("p");
+        let newCloseBtn = document.createElement("button");
+        //Configuration du boutton
+        newCloseBtn.innerText = "X";
+        newCloseBtn.classList.add("close-btn");
+        //Setup de la div
+        newdivElt.classList.add("item");
+        //Setup du p
+        newPElt.innerText = `Je suis ${contact.name} ${contact.lastname} mon numéro est le ${contact.phone}, et voila mon adresse mail ${contact.mail}`;
+        //Injection des elements dans la nouvelle div
+        newdivElt.appendChild(newPElt);
+        newdivElt.appendChild(newCloseBtn);
+        //Injection de la div dans la grosse div
+        contactsElt.appendChild(newdivElt);
+        //Event au click sur le boutton fermé
+        newCloseBtn.addEventListener("click", () => {
+            //suppression de l'element dans le array
+            //Recherche de l'index du contact grace a l'id de l'user
+            const findById = (element) => element.id == contact.id;
+            let index = contacts.findIndex(findById);
+            //Suppression de l'élément dans l'array grace a l'index
+            contacts.splice(index, 1);
+            //Mise a Jour dans le localStorage
+            saveOnLocal();
+            //suppression de la div
+            newdivElt.remove()
+        }) 
+    });
+};
+//Fonction de sauvegarde en local
+function saveOnLocal(){
+    //Stingification de l'array
+    let contactsString = JSON.stringify(contacts);
+    //Sauvegarde en localStorage
+    localStorage.setItem("contact",contactsString);
+    //debug Perso
+    console.log(localStorage.getItem("contact"));
+}
+//Event au click sur le btn valider
+validerBtn.addEventListener('click', function(){
+    //longueur de l'array
+    let listLength = contacts.length;
+    //creation d'un objet
+    let newEntry = {
+        "id" : listLength,
+        "name" : nameInputElt.value,
+        "lastname" : lastnameInputElt.value,
+        "mail" : mailInputElt.value,
+        "phone" : telInputElt.value
+    };
+    //push de l'objet dans l'array contacts
+    contacts.push(newEntry);    
+    //Sauvegarde dans le localStorage
+    saveOnLocal()
+    //Reset de la div principale
+    contactsElt.innerHTML = "";
+    //Boucle sur l'array (pour chaque)
+    contacts.forEach((contact) => {
+        //Création des elements   
+        let newdivElt = document.createElement("div");
+        let newPElt = document.createElement("p");
+        let newCloseBtn = document.createElement("button");
+        //Configuration du boutton
+        newCloseBtn.innerText = "X";
+        newCloseBtn.classList.add("close-btn");
+        //Setup de la div
+        newdivElt.classList.add("item");
+        //Setup du p
+        newPElt.innerText = `Je suis ${contact.name} ${contact.lastname} mon numéro est le ${contact.phone}, et voila mon adresse mail ${contact.mail}`;
+        //Injection des elements dans la nouvelle div
+        newdivElt.appendChild(newPElt);
+        newdivElt.appendChild(newCloseBtn);
+        //Injection de la div dans la grosse div
+        contactsElt.appendChild(newdivElt);
+        //Event au click sur le boutton fermé
+        newCloseBtn.addEventListener("click", () => {
+            //suppression de l'element dans le array
+            //Recherche de l'index du contact grace a l'id de l'user
+            const findById = (element) => element.id == contact.id;
+            let index = contacts.findIndex(findById);
+            //Suppression de l'élément dans l'array grace a l'index
+            contacts.splice(index, 1);
+            //Mise a Jour dans le localStorage
+            saveOnLocal();
+            //suppression de la div
+            newdivElt.remove()
+        }) 
+    });
+    //reset des champs
+    nameInputElt.value = "";
+    lastnameInputElt.value = "";
+    mailInputElt.value = "";
+    telInputElt.value = "";
+});
+
+
+
+findInputElt.addEventListener("input", () => {
+    let filteredContacts = "";
+    function verifierCorrespondance(contact) {
+        let regex = new RegExp(`${findInputElt.value}`);
+        let result = regex.test(contact.name);
+        if (result) {
+            return contact;
+        }
+    }    
+    filteredContacts = contacts.filter(verifierCorrespondance);
+    console.log(filteredContacts);
+
+    //reset la main div
+    contactsElt.innerHTML="";
+    filteredContacts.forEach((contact) => {
+        //Création des elements   
+        let newdivElt = document.createElement("div");
+        let newPElt = document.createElement("p");
+        let newCloseBtn = document.createElement("button");
+        //Configuration du boutton
+        newCloseBtn.innerText = "X";
+        newCloseBtn.classList.add("close-btn");
+        //Setup de la div
+        newdivElt.classList.add("item");
+        //Setup du p
+        newPElt.innerText = `Je suis ${contact.name} ${contact.lastname} mon numéro est le ${contact.phone}, et voila mon adresse mail ${contact.mail}`;
+        //Injection des elements dans la nouvelle div
+        newdivElt.appendChild(newPElt);
+        newdivElt.appendChild(newCloseBtn);
+        //Injection de la div dans la grosse div
+        contactsElt.appendChild(newdivElt);
+        //Event au click sur le boutton fermé
+        newCloseBtn.addEventListener("click", () => {
+            //suppression de l'element dans le array
+            //Recherche de l'index du contact grace a l'id de l'user
+            const findById = (element) => element.id == contact.id;
+            let index = contacts.findIndex(findById);
+            //Suppression de l'élément dans l'array grace a l'index
+            contacts.splice(index, 1);
+            //Mise a Jour dans le localStorage
+            saveOnLocal();
+            //suppression de la div
+            newdivElt.remove()
+        }) 
+    });
+});
+
+findInputElt.addEventListener('blur', () => {
+    findInputElt.value = "";
+    contacts.forEach((contact) => {
+        //Création des elements   
+        let newdivElt = document.createElement("div");
+        let newPElt = document.createElement("p");
+        let newCloseBtn = document.createElement("button");
+        //Configuration du boutton
+        newCloseBtn.innerText = "X";
+        newCloseBtn.classList.add("close-btn");
+        //Setup de la div
+        newdivElt.classList.add("item");
+        //Setup du p
+        newPElt.innerText = `Je suis ${contact.name} ${contact.lastname} mon numéro est le ${contact.phone}, et voila mon adresse mail ${contact.mail}`;
+        //Injection des elements dans la nouvelle div
+        newdivElt.appendChild(newPElt);
+        newdivElt.appendChild(newCloseBtn);
+        //Injection de la div dans la grosse div
+        contactsElt.appendChild(newdivElt);
+        //Event au click sur le boutton fermé
+        newCloseBtn.addEventListener("click", () => {
+            //suppression de l'element dans le array
+            //Recherche de l'index du contact grace a l'id de l'user
+            const findById = (element) => element.id == contact.id;
+            let index = contacts.findIndex(findById);
+            //Suppression de l'élément dans l'array grace a l'index
+            contacts.splice(index, 1);
+            //Mise a Jour dans le localStorage
+            saveOnLocal();
+            //suppression de la div
+            newdivElt.remove()
+        }) 
+    });
+    //reset la main div
+    //refaire des sous div avec les contacts du localstorage
+})
